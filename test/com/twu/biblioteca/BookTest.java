80a927e6def3d07d6a3a35208896641e1f0242f5
Harini V S <harinivs@thoughtworks.com>
2017-06-15 18:20:13 +0530

    Test the attributes of Book class

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,37 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+    @Test
+    public void shouldReturnBookName() {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
+
+        assertEquals("The Alchemist", book.getName());
+    }
+
+    @Test
+    public void shouldReturnAuthorName() {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
+
+        assertEquals("Paulo Coelho", book.getAuthor());
+    }
+
+    @Test
+    public void shouldReturnPublicationYear() {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
+
+        assertEquals(1988, book.getPublicationYear());
+    }
+
+    @Test
+    public void shouldReturnABook() {
+        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
+
+        assertEquals("Book name = The Alchemist, Author = Paulo Coelho, Year of publication = 1988", book.toString());
+    }
+
+}
