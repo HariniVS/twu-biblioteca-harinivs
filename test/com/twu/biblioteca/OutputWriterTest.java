6447df03e16406f0487d42ceee4dfa4f22542e3c
Harini V S <harinivs@thoughtworks.com>
2017-06-16 11:13:18 +0530

    Test the output writer

---
+++ test/com/twu/biblioteca/OutputWriterTest.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+
+public class OutputWriterTest {
+    private StringWriter stringWriter;
+    private OutputWriter outputWriter;
+
+    @Test
+    public void shouldDisplayToConsole() {
+        stringWriter = new StringWriter();
+        outputWriter = new OutputWriter(stringWriter);
+        outputWriter.write("Bangalore Public Library Welcomes " +
+                        "You");
+        assertEquals("Bangalore Public Library Welcomes You\n",stringWriter
+                .getBuffer().toString());
+    }
+}
